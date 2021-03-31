d5c22bfe5b4195321d3617a1e452ee975640fa20
misoshiru-code <79170524+misoshiru-code@users.noreply.github.com>
2021-03-31 10:55:14 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
-$(function($){
-    var log = '';
-    log += 'window.innerHeight = ' + window.innerHeight + '  ';
-    log += 'window.outerHeight = ' + window.outerHeight + '  ';
-    log += '100vh = ' + $('#vh').height() + '  ';
-    log += 'screen.height = ' + screen.height + '  ';
-    $('#vh').prepend(log);
+$(function ($) {
+
+  $('.inner-height').text('innerHeight = ' + window.innerHeight);
+  $('.outer-height').text('outerHeight = ' + window.outerHeight);
+  $('.vh').text('100vh = ' + $('#vh').height());
+  $('.screen-height').text('screen.height = ' + screen.height);
+
+  if (window.innerHeight == screen.height) {
+    $('.answer').text('同じ値です');
+  } else {
+    $('.answer').text('違う値です');
+  }
+
+  $(window).resize(function () {
+
+    $('.inner-height').text('innerHeight = ' + window.innerHeight);
+    $('.outer-height').text('outerHeight = ' + window.outerHeight);
+    $('.vh').text('100vh = ' + $('#vh').height());
+    $('.screen-height').text('screen.height = ' + screen.height);
+
+    if (window.innerHeight == screen.height) {
+      $('.answer').text('同じ値です');
+    } else {
+      $('.answer').text('違う値です');
+    }
   });
+});
