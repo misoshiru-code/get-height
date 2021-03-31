0c271880194f892f7f7fc4a70e7ccb4ec1424068
misoshiru-code <79170524+misoshiru-code@users.noreply.github.com>
2021-03-31 16:33:24 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ $(function ($) {
   $(window).resize(function () {
 
     $('.inner-height').text('innerHeight = ' + window.innerHeight);
-    /*$('.outer-height').text('outerHeight = ' + window.outerHeight);
+    $('.outer-height').text('outerHeight = ' + window.outerHeight);
     $('.vh').text('100vh = ' + $('#vh').height());
     $('.screen-height').text('screen.height = ' + screen.height);
 
@@ -22,7 +22,7 @@ $(function ($) {
       $('.answer').text('同じ値です');
     } else {
       $('.answer').text('違う値です');
-    }*/
+    }
   });
 
 });
